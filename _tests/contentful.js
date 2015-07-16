c182a1901bfa48463ca90655007acfb07e571dcb
Matt Finucane <matfin@gmail.com>
2015-07-16 22:40:31 +0200

    Adding more tests for the Contentful class.

--- _tests/contentful.js
+++ _tests/contentful.js
@@ -8,6 +8,10 @@ Tinytest.add('Contentful - checkCFCredentails should return true if the header a
 		}
 	};
 	CFConfig.callbackAuthKey = '123456';
+
+	/**
+	 *	Running the test
+	 */
 	test.equal(Contentful.checkCFCredentials(request), true);
 
 });
@@ -24,6 +28,64 @@ Tinytest.add('Contentful - checkCFCredentails should return false if the header 
 		}
 	};
 	CFConfig.callbackAuthKey = '123456';
+	
+	/**
+	 *	Running the test
+	 */
 	test.equal(Contentful.checkCFCredentials(request), false);
+
+});
+
+
+Tinytest.add('Contentful - contentTypeName should return the correctly matching name of a content type given its id.', function(test) {
+
+	/**
+	 *	Stubbing objects for the test
+	 */
+	var entry = {
+		sys: {
+			contentType: {
+				sys: {
+					id: '12345'
+				}
+			}
+		}
+	};
+	/**
+	 *	Adding in a test content type name
+	 */
+	CFConfig.contentTypes.push({
+		'name': 'testName',
+		'id': '12345'
+	});
+
+	test.equal(Contentful.contentTypeName(entry), 'testName');
+
+});
+
+
+Tinytest.add('Contentful - contentTypeName should return the string \'nested\' if no content type name is found.', function(test) {
+
+	/**
+	 *	Stubbing objects for the test
+	 */
+	var entry = {
+		sys: {
+			contentType: {
+				sys: {
+					id: '123456789'
+				}
+			}
+		}
+	};
+	/**
+	 *	Adding in a test content type name
+	 */
+	CFConfig.contentTypes.push({
+		'name': 'testName',
+		'id': '11'
+	});
+
+	test.equal(Contentful.contentTypeName(entry), 'nested');
 
 });
